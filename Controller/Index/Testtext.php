d959f1bf16ee2847c429885d0e7965818b311684
SoulBeast <WweveW@mail.ru>
2023-07-10 15:00:37 +0300

    Theme 2 First module

---
+++ Controller/Index/Testtext.php
@@ -0,0 +1,11 @@
+<?php
+namespace Perspective\HelloWorld\Controller\Index;
+use Magento\Framework\App\Action\Action;
+class Testtext extends Action
+{
+    public function execute()
+    {
+        die("Controller1_1");
+    }
+}
+?>
